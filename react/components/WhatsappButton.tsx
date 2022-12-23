fbff10008fd3763a13920c7e7dc4b0a1928dae49
DaybelCR <dyi.cisnerosr@gmail.com>
2022-12-22 22:57:55 -0500

    feat: add css handles for custom button

--- react/components/WhatsappButton.tsx
+++ react/components/WhatsappButton.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
+import {useCssHandles} from 'vtex.css-handles';
+
 
 type Props={
     logo:string
@@ -11,16 +13,20 @@ type Props={
 
 const WhatsappButton=({logo,phone,message,width,height}: Props)=>{
     const formattedMessage=message.replace(/ /g,'%20')
-    console.log('formated',formattedMessage)
-    console.log(logo,phone,message)
-    return <>
-           <div className="fixed bottom-2 right-2 flex flexColumn">
+    const CSS_HANDLES = [
+        'container-logo--wtsp',
+        'image-logo--wtsp',
+      ]
+      const handles = useCssHandles(CSS_HANDLES)
+    return <>  
+           <div className={`${handles['container-logo--wtsp']} fixed bottom-2 right-2 flex flexColumn`}>
            <a
              href={`https://wa.me/${phone}?text=${formattedMessage}`}
              target="_blank"
              rel="noreferrer noopener"
              >
                 <img
+                  className={`${handles['image-logo--wtsp']}`}
                   src={logo}
                   width={width}
                   height={height}
@@ -38,9 +44,9 @@ WhatsappButton.prototype={
     height:PropTypes.number
 }
 WhatsappButton.defaultProps={
-    logo: "test.logo.png",
-    phone: "93423568",
-    message: "Hola,bienvenido a la tienda",
+    logo: "test-logo.png",
+    phone: "1234567",
+    message: "Bienvenido(a) a la tienda,no dude en preguntar!",
     width:80,
     height:80
 }
@@ -69,12 +75,12 @@ WhatsappButton.schema={
             }
         },
         width:{
-            title:"width logo",
+            title:"ancho del logo",
             description:"Agrega por favor el ancho del logo",
             type: "number",
         },
         height:{
-            title:"height logo",
+            title:"alto del logo",
             description:"Agrega por favor la altura del logo",
             type: "number",
         }
